c135fc25d67383b9cbaf384a745786844291c6e7
Piotr Budnicki <32633831+ntguilty@users.noreply.github.com>
2020-04-09 13:25:12 +0200

    update App.js

--- mydockerfrontend/src/App.js
+++ mydockerfrontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Welcome!
+          Edit <code>src/App.js</code> and Welcome CI!
         </p>
         <a
           className="App-link"
